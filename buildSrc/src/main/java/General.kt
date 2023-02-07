2d761de628162f851f226a133b691e4815f66203
Dmitry Popov <lyrjie@gmail.com>
2023-02-07 22:50:26 +0300

    Remove TODO

--- buildSrc/src/main/java/General.kt
+++ buildSrc/src/main/java/General.kt
@@ -1,9 +1,7 @@
 const val appName = "Bloody Simple Tracker"
 
-// TODO Does your project use compose?
 const val isComposeProject = true
 
-// TODO Do you need Room?
 const val isRoomEnabled = false
 
 const val signingConfigName = "default"
