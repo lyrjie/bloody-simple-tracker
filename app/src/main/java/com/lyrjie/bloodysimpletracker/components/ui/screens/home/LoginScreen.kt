b52a8ec73020d4dca77318214a92764c61bc0205
Dmitry Popov <lyrjie@gmail.com>
2023-02-09 23:50:21 +0300

    Add login button to login screen

--- app/src/main/java/com/lyrjie/bloodysimpletracker/components/ui/screens/home/LoginScreen.kt
+++ app/src/main/java/com/lyrjie/bloodysimpletracker/components/ui/screens/home/LoginScreen.kt
@@ -1,9 +1,39 @@
 package com.lyrjie.bloodysimpletracker.components.ui.screens.home
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.lyrjie.bloodysimpletracker.components.ui.common.placeholder.ScreenPlaceholder
+import com.lyrjie.bloodysimpletracker.components.ui.theme.TrackerTheme
+import com.lyrjie.bloodysimpletracker.components.ui.tooling.ThemePreviews
 
 @Composable
-fun LoginScreen() {
-    ScreenPlaceholder(title = "Login")
+fun LoginScreen(
+    showHome: () -> Unit,
+) {
+    ScreenPlaceholder(
+        title = "Login",
+    ) {
+        Button(
+            onClick = showHome,
+        ) {
+            Text(
+                text = "Login",
+                modifier = Modifier.padding(horizontal = 32.dp, vertical = 16.dp)
+            )
+        }
+    }
+}
+
+@ThemePreviews
+@Composable
+private fun LoginScreenPreview() {
+    TrackerTheme {
+        LoginScreen(
+            showHome = {}
+        )
+    }
 }
