1a26aacbd2ed9760142d5d5f3ddab8e3aef8c492
Dmitry Popov <lyrjie@gmail.com>
2023-02-09 23:42:24 +0300

    Add home and login screen placeholders

---
+++ app/src/main/java/com/lyrjie/bloodysimpletracker/components/ui/screens/home/LoginScreen.kt
@@ -0,0 +1,9 @@
+package com.lyrjie.bloodysimpletracker.components.ui.screens.home
+
+import androidx.compose.runtime.Composable
+import com.lyrjie.bloodysimpletracker.components.ui.common.placeholder.ScreenPlaceholder
+
+@Composable
+fun LoginScreen() {
+    ScreenPlaceholder(title = "Login")
+}
