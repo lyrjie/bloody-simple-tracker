b5e71645dbef7ee9c0a419af6d19a4d6b6144ff9
Dmitry Popov <lyrjie@gmail.com>
2023-02-09 23:02:35 +0300

    Use splash screen theme for startup

--- app/src/main/java/com/lyrjie/bloodysimpletracker/components/ui/MainActivity.kt
+++ app/src/main/java/com/lyrjie/bloodysimpletracker/components/ui/MainActivity.kt
@@ -3,12 +3,14 @@ package com.lyrjie.bloodysimpletracker.components.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
 
         WindowCompat.setDecorFitsSystemWindows(window, false)
