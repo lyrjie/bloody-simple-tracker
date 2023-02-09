022cef4900d925aaffc310ca0dfdac0df913e1e1
Dmitry Popov <lyrjie@gmail.com>
2023-02-09 22:19:51 +0300

    Make window edge-to-edge

--- app/src/main/java/com/lyrjie/bloodysimpletracker/components/ui/MainActivity.kt
+++ app/src/main/java/com/lyrjie/bloodysimpletracker/components/ui/MainActivity.kt
@@ -3,12 +3,16 @@ package com.lyrjie.bloodysimpletracker.components.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.view.WindowCompat
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
         setContent {
             BloodSimpleTrackerApp()
         }
